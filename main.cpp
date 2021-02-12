1d7a5df079015b32273c9806d4d306c6896c4b48
edgarasratas <73912309+edgarasratas@users.noreply.github.com>
2021-02-12 17:52:52 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char frame = '*';
+    string greeting = "Sveikas, ";
+    string name;
+
+    //asks the user to input his name
+    cout << "Iveskite savo varda:\n";
+    cin >> name;
+    cout << endl;
+
+    greeting += name;
+
+    //prints the first line
+    for (int i = 0; i < greeting.length() + 5; i++)
+    {
+        cout << frame;
+    }
+
+    cout << endl;
+
+    //prints the second line
+    for (int i = 1; i <= greeting.length() + 5; i++)
+    {
+        if ((i == 1) || (i == greeting.length() + 5)) {
+            cout << frame;
+        }
+        else {
+            cout << ' ';
+        }
+    }
+
+    cout << endl;
+
+    //prints the third line
+    cout << "* " << greeting << "! *\n";
+
+    //prints the fourth line
+    for (int i = 1; i <= greeting.length() + 5; i++)
+    {
+        if ((i == 1) || (i == greeting.length() + 5)) {
+            cout << frame;
+        }
+        else {
+            cout << ' ';
+        }
+    }
+
+    cout << endl;
+
+    //prints the fifth line
+    for (int i = 0; i < greeting.length() + 5; i++)
+    {
+        cout << frame;
+    }
+}
