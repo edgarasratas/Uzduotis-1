fb9370212548f13d4f99036a8642f43013673746
edgarasratas <73912309+edgarasratas@users.noreply.github.com>
2021-02-12 18:51:43 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 ﻿#include <iostream>
-#include <string>
 
 using namespace std;
-
-bool endsWith(string str, string ending);
 
 int main()
 {
@@ -15,13 +12,6 @@ int main()
     cout << "Iveskite savo varda:\n";
     cin >> name;
     cout << endl;
-
-    if (endsWith(name, "s")) {
-        greeting = "Sveikas, ";
-    }
-    else if (endsWith(name, "a")) {
-        greeting = "Sveika, ";
-    }
 
     greeting += name;
 
@@ -66,15 +56,5 @@ int main()
     for (int i = 0; i < greeting.length() + 5; i++)
     {
         cout << frame;
-    }
-}
-
-
-bool endsWith(string str, string ending) {
-    if (str.length() >= ending.length()) {
-        return (0 == str.compare(str.length() - ending.length(), ending.length(), ending));
-    }
-    else {
-        return false;
     }
 }
